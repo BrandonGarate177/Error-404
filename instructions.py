c519563739bef599131b3bc41ef88dd17c6f35b7
BrandonGarate177 <brandongarate177@gmail.com>
2025-05-04 16:29:10 -0700

    Elegant compiler

--- instructions.py
+++ instructions.py
@@ -5,15 +5,13 @@ INSTRUCTIONS = {
     "MUL":     {"type": "R", "opcode": "000000", "funct": "011000"},
     "DIV":     {"type": "R", "opcode": "000000", "funct": "011010"},
     "MOD":     {"type": "R", "opcode": "000000", "funct": "011011"},
-    
     "CRASH":   {"type": "R", "opcode": "010010", "funct": "000000"},
     "PANIC":   {"type": "R", "opcode": "010100", "funct": "000001"},
     "FREEZE":  {"type": "R", "opcode": "010101", "funct": "000000"},
     "NULL":    {"type": "R", "opcode": "010110", "funct": "000001"},
     "TRACE":   {"type": "R", "opcode": "010111", "funct": "000000"},
     "FORK":    {"type": "R", "opcode": "011000", "funct": "000001"},
-    "BREAKPT": {"type": "R", "opcode": "011010", "funct": "000000"},
-
+    "BREAKPT": {"type": "R", "opcode": "011010", "funct": "000000"},    
     # I-Type Instructions
     "ADDI":    {"type": "I", "opcode": "001000"},
     "BEQ":     {"type": "I", "opcode": "000100"},
@@ -21,14 +19,9 @@ INSTRUCTIONS = {
     "LW":      {"type": "I", "opcode": "100011"},
     "SW":      {"type": "I", "opcode": "101011"},
     "BLT":     {"type": "I", "opcode": "000110"},  
-    
     "404":     {"type": "I", "opcode": "010001"},  # Branch if R2 == -1
     "GLITCH":  {"type": "I", "opcode": "011001"},  # Randomly swap R1 and R2
-
     # J-Type Instructions
     "REBOOT":  {"type": "J", "opcode": "010011"},
-    "JUMP": {"type": "J", "opcode": "000010"},  # Classic MIPS-style jump
-
+    "JUMP":    {"type": "J", "opcode": "000010"},
 }
-
-
---
+++ output.asm
@@ -0,0 +1,48 @@
+ADDI $r2, $r0, 1
+ADD $r1, $r2, $r0
+JUMP i <= 15_CHECK
+LOOP0:
+ADDI $r3, $r0, 3
+MOD $r4, $r1, $r3
+ADDI $r5, $r0, 0
+BEQ $r4, $r5, AND4
+ADDI $r6, $r0, 5
+MOD $r7, $r1, $r6
+ADDI $r8, $r0, 0
+BEQ $r7, $r8, IF_T2
+AND4:
+JUMP IF_E3
+IF_T2:
+ADDI $r6, $r0, 70
+PANIC $r6
+ADDI $r6, $r0, 105
+PANIC $r6
+ADDI $r6, $r0, 122
+PANIC $r6
+ADDI $r6, $r0, 122
+PANIC $r6
+ADDI $r6, $r0, 66
+PANIC $r6
+ADDI $r6, $r0, 117
+PANIC $r6
+ADDI $r6, $r0, 122
+PANIC $r6
+ADDI $r6, $r0, 122
+PANIC $r6
+ADDI $r6, $r0, 70
+PANIC $r6
+ADDI $r6, $r0, 105
+PANIC $r6
+ADDI $r6, $r0, 122
+PANIC $r6
+ADDI $r6, $r0, 122
+PANIC $r6
+ADDI $r6, $r0, 66
+PANIC $r6
+ADDI $r6, $r0, 117
+PANIC $r6
+ADDI $r6, $r0, 122
+PANIC $r6
+ADDI $r6, $r0, 122
+PANIC $r6
+IF_E3:
